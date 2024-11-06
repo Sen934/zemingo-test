db16f99c9259b7f812bc908bcf4b5a3ed9431db1
Sen934 <46794874+Sen934@users.noreply.github.com>
2024-11-06 15:19:41 +0100

    Add product select component

---
+++ src/components/common/ProductSelect.tsx
@@ -0,0 +1,53 @@
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { useProducts } from "../../api/hooks/useProducts";
+
+export const ProductSelect: React.FC = () => {
+  const { products, isProductsLoading } = useProducts();
+  const { control, formState } = useFormContext<{ productName: string }>();
+
+  const errors = formState.errors;
+
+  if (!products) {
+    return null;
+  }
+
+  return (
+    <FormControl variant="outlined" error={!!errors.productName}>
+      <InputLabel id="product-name-select-label">Product name</InputLabel>
+      <Controller
+        name="productName"
+        control={control}
+        rules={{ required: "Country is required" }}
+        disabled={isProductsLoading}
+        render={({ field }) => (
+          <Select
+            {...field}
+            label="Please select product name"
+            id="product-name-select"
+            labelId="product-name-select-label"
+            sx={{ minWidth: 200 }}
+            value={""}
+          >
+            {isProductsLoading && <MenuItem>Loading...</MenuItem>}
+            {products.map(({ name }) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        )}
+      />
+      {errors.productName && (
+        <FormHelperText>{errors.productName?.message}</FormHelperText>
+      )}
+    </FormControl>
+  );
+};
